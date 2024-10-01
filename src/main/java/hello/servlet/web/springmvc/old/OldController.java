9c90423efa503b8d5c571ebb26561718c2f66451
HarryBae1011 <06harry@naver.com>
2024-10-01 12:03:39 +0900

    핸들러 매핑과 핸들러 어댑터

---
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,18 @@
+package hello.servlet.web.springmvc.old;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        //호출이 되는지만 확인
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
